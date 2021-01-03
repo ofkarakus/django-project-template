de3a40bd09e61deb32ebf085a3fcee25d6122816
ofkarakus <omerfarukkarakus01@gmail.com>
2021-01-03 19:50:15 +0000

    create, read, delete operations

--- src/first_app/urls.py
+++ src/first_app/urls.py
@@ -2,7 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/', views.display_home_page),
-    path('about/', views.display_about_page),
-    path('add/', views.display_addstudent_page)
+    path('home/', views.display_home_page, name='Home Page'),
+    path('about/', views.display_about_page, name='About Page'),
+    path('form/', views.display_form, name='Form Page'),
+    path('list/', views.get_student_list, name='List Page'),
+    path('add/', views.add_student, name='Add Page'),
+    path('<int:id>/detail/', views.get_student_details, name='Detail Page'),
+    path('<int:id>/delete/', views.delete_student, name='Delete Student')
 ]
--- src/first_app/views.py
+++ src/first_app/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render, redirect
 from django.http import HttpResponse
-
+from first_app.models import Student
 from first_app.forms import StudentForm
 
 # Create your views here.
@@ -24,7 +24,7 @@ def display_about_page(request):
     return HttpResponse("About Page")
 
 
-def display_addstudent_page(request):
+def display_form(request):
     form = StudentForm()
     html = 'Welcome'
     if request.method == 'POST':
@@ -36,4 +36,41 @@ def display_addstudent_page(request):
         'form': form,
         'html': html
     }
-    return render(request, 'first_app/student_add.html', context)
+    return render(request, 'first_app/form.html', context)
+
+
+def get_student_list(request):
+    students = Student.objects.all()
+    context = {
+        'students': students
+    }
+    return render(request, 'first_app/student_list.html', context)
+
+
+def add_student(request):
+    form = StudentForm()
+    if request.method == "POST":
+        form = StudentForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("List Page")
+    context = {
+        'form': form,
+    }
+    return render(request, 'first_app/add_student.html', context)
+
+
+def get_student_details(request, id):
+    student = Student.objects.get(id=id)
+    context = {
+        'student': student
+    }
+    return render(request, 'first_app/student_detail.html', context)
+
+
+def delete_student(request, id):
+    student = get_object_or_404(Student, id=id)
+    if request.method == 'POST':
+        student.delete()
+        return redirect('List Page')
+    return render(request, 'first_app/delete_student.html')
