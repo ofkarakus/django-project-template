ab4d988fc69141cf075e56a68c765588912faf0b
Omer Karakus <omerfarukkarakus01@gmail.com>
2021-01-23 12:44:43 +0000

    add students API by for loop + append() method

--- src/django_rest_app/urls.py
+++ src/django_rest_app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/', views.home_api)
+    path('home/', views.home_api),
+    path('student-list-1/', views.student_list_api_1),
 ]
--- src/django_rest_app/views.py
+++ src/django_rest_app/views.py
@@ -1,13 +1,35 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from first_app.models import Student
 
 # Create your views here.
 
 
 def home_api(request):
-     data = {
-         'first_name': 'oscar',
-         'last_name': 'thomson',
-         'skills': ['python', 'django']
-     }
-     return JsonResponse(data)
+    data = {
+        'first_name': 'oscar',
+        'last_name': 'thomson',
+        'skills': ['python', 'django']
+    }
+    return JsonResponse(data)
+
+
+# by for loop + append() method
+def student_list_api_1(request):
+    if request.method == 'GET':
+        students = Student.objects.all()
+        student_count = Student.objects.count()
+
+        student_list = []
+        for student in students:
+            student_list.append({
+                'first_name': student.first_name,
+                'last_name': student.last_name,
+                'number': student.number
+            })
+
+        data = {
+            'students': student_list,
+            'student_count': student_count
+        }
+        return JsonResponse(data)
